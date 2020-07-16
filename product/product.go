7f656332480e4a96cd548076ae7b9317e08a9431
SINGH Dharmjit TGI/OLS <dharmjit.singh@orange.com>
2020-07-16 20:17:51 +0530

    Product and User Struct defined

---
+++ product/product.go
@@ -0,0 +1,11 @@
+package product
+
+//CONSTANT
+//ENUM
+//CUSTOM TYPE
+//VALIDATION
+type Product struct {
+	ProductName     string
+	ProductCategory string // ELECTRONICS,BEAUTY,GARMENT
+	ProductPrice    float32
+}
